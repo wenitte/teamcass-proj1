d64c87ecf976e581dfbc7b924cb638a6625d580c
adamhanna <adamhannadev@gmail.com>
2020-05-28 11:31:58 -0700

    Create upload-midi page and move Landing into components folder

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -106,9 +106,9 @@ export default function Dashboard() {
                             <Grid item sm={6} style={{ paddingTop: theme.spacing(15) }}>
                                 <Typography variant="h2">Hello, adamhannadev!</Typography>
                                 <form className={classes.form} noValidate autoComplete="off">
-                                    <TextField id="outlined-basic" label="Outlined" variant="outlined" fullWidth label="User Name" />
-                                    <TextField id="outlined-basic" label="Outlined" variant="outlined" fullWidth label="Email" />
-                                    <TextField id="outlined-basic" label="Outlined" variant="outlined" fullWidth label="Password" />
+                                    <TextField id="name" label="Outlined" variant="outlined" fullWidth label="User Name" />
+                                    <TextField id="email" label="Outlined" variant="outlined" fullWidth label="Email" />
+                                    <TextField id="phone" label="Outlined" variant="outlined" fullWidth label="Password" />
                                     <Button variant="contained" color="primary">
                                         Update
                                 </Button>
