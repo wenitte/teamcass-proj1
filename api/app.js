3fa90b79719743a173e7d5f0bdb47a9831a4b193
adamhanna <adamhannadev@gmail.com>
2020-06-01 13:39:31 -0700

    Add upload file function to /upload/:title

--- api/app.js
+++ api/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,17 +14,53 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+
+// enable file upload
+app.use(fileUpload({
+  createParentPath: true
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/mix', mixRouter);
+
+app.post('/upload', async (req, res) => {
+  try {
+    if (!req.files) {
+      res.send({
+        status: false,
+        message: 'No file uploaded'
+      });
+    } else {
+      //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+      let recording = req.files.recording;
+
+      //Use the mv() method to place the file in upload directory (i.e. "uploads")
+      recording.mv(`./public/audio/` + req.body.title + `/ ` + req.body.title + `-` + req.body.part + `.mp3`);
+
+      //send response
+      res.send({
+        status: true,
+        message: 'File is uploaded',
+        data: {
+          mimetype: recording.mimetype,
+          size: recording.size
+        }
+      });
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
