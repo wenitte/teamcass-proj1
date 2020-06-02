6b03973d57473dbe50921cb2b997d88b6f1591bf
adamhanna <adamhannadev@gmail.com>
2020-06-02 10:59:42 -0700

    Enable starting front and backend servers with 'yarn dev'

--- api/app.js
+++ api/app.js
@@ -79,6 +79,6 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000, function () {
-  console.log('CORS-enabled web server listening on port 3000');
+app.listen(8000, function () {
+  console.log('CORS-enabled web server listening on port 8000');
 })
