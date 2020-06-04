a3c79693e89a869a9683a7f1421d893e0557ca8f
adamhanna <adamhannadev@gmail.com>
2020-06-04 12:17:41 -0700

    Add function to move uploaded files temporaily, mix and then pipe back the mixed file

--- api/routes/mix.js
+++ api/routes/mix.js
@@ -2,18 +2,47 @@ var express = require('express');
 var router = express.Router();
 const { exec } = require("child_process");
 
-router.get('/:title', function (req, res, next) {
+router.post('/', function (req, res, next) {
 
+    let recordings = req.body.recordings;
+    let paths = [];
+    for (let i = 0; i < recordings.length; i++) {
+        let rec = recordings[i];
+        paths.push(`public/recordings/${rec.songID}/${rec.partID}/${rec.uid}-${rec.songID}-${rec.partID}.mp3`);
+    }
+    console.log(paths);
     // Send a request to /mix/{the song title from the record song page}
     // Change directories into the uploaded song files folder
     // Use the Sound Exchange library to mix all 4 parts into one part
+    for (let i = 0; i < paths.length; i++) {
+        let mvCmd = exec(`cp ${paths[i]} public/recordings/tmp`
+            , (error, stdout, stderr) => {
+                if (error) {
+                    console.log(`error: ${error.message}`);
+                    return;
+                }
+                if (stderr) {
+                    console.log(`stderr: ${stderr}`);
+                    return;
+                }
+                console.log(`stdout: ${stdout}`);
+            });
+        mvCmd.on('exit', function (code) {
+            console.log('Child process exited with exit code ' + code);
+        });
+    }
 
-    let title = req.params.title;
+
+    let title = req.body.recordings[0].songID;
+    let currentTime = Date.now();
+    let mixedFile = `${title}-${currentTime}-mixed.mp3`;
     const mixCmd = exec(`
-    set -e &&
-    cd public/audio/${title}/ && 
-    sox -m *.mp3 ${title}-mixed.mp3 &&
-    echo ${title}-mixed.mp3 has been created.
+    set - e &&
+    cd public/recordings/tmp/ &&
+    sox -m *.mp3 ${title}-${currentTime}-mixed.mp3 &&
+    mv  ${mixedFile} ../mixed/ &&
+    rm * &&
+    echo ${ title} - mixed.mp3 has been created.
     `, (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`);
@@ -28,9 +57,9 @@ router.get('/:title', function (req, res, next) {
     mixCmd.on('exit', function (code) {
         console.log('Child process exited with exit code ' + code);
     });
-    res.json({ message: `Created new audio file - ${title}-mixed.mp3` });
+    res.json({ "url": `/recordings/mixed/${mixedFile}` });
 });
 
 module.exports = router;
 
-// 
+// api/public/midi /midi-files
