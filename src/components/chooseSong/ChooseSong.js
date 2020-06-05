1a03183366a34df9eb29eeb3bd7180e6606ded8a
Timothy Liu <timsgithub@gmail.com>
2020-06-05 13:34:14 -0700

    Update ChooseSong UI

--- src/components/chooseSong/ChooseSong.js
+++ src/components/chooseSong/ChooseSong.js
@@ -2,12 +2,10 @@ import React from "react";
 import Container from "@material-ui/core/Container";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { makeStyles } from "@material-ui/core/styles";
-import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 import Button from "@material-ui/core/Button";
-import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles(theme => ({
@@ -18,11 +16,8 @@ const useStyles = makeStyles(theme => ({
     alignItems: "center"
   },
   formControl: {
-    margin: theme.spacing(1),
+    margin: theme.spacing(8),
     minWidth: 200
-  },
-  selectEmpty: {
-    marginTop: theme.spacing(2)
   },
   buttonGroup: {
     margin: theme.spacing(2)
@@ -41,8 +36,8 @@ export default function ChooseSong() {
     <Container>
       <CssBaseline />
       <div className={classes.paper}>
+        <Typography variant="h3">Choose your song</Typography>
         <FormControl className={classes.formControl}>
-          <InputLabel>Choose Song</InputLabel>
           <Select label="" value={song} onChange={handleChange}>
             <MenuItem value="">
               <em>None</em>
@@ -53,19 +48,11 @@ export default function ChooseSong() {
           </Select>
         </FormControl>
       </div>
-      <div className={classes.buttonGroup}>
-        <Typography variant="h6">Choose Role</Typography>
-        <ButtonGroup
-          size="large"
-          color="primary"
-          aria-label="outlined primary button group"
-        >
-          <Button>Soprano</Button>
-          <Button>Tenor</Button>
-          <Button>Alto</Button>
-          <Button>Bass</Button>
-        </ButtonGroup>
-      </div>
+      
+      <Button variant="contained" color="primary" size="large">
+        Let's go
+      </Button>
+      
     </Container>
   );
 }
