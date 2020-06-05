737d79f5ca62c1ca572ca1f31604d20349941e1f
Pranav Mahesh <pmahesh@gatech.edu>
2020-06-05 11:55:38 -0400

    Remove unnecessary imports from navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,18 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
-import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import './style.css'
 import { signOutStart } from "../../redux/user/user.actions";
+import './style.css'
 
 const Navbar = ({ signOut, currentUser }) => {
   return (
     <div class="navbar">
     <Grid justify="space-between" container spacing={3}>
       <Grid item>
-      <img src="../quartetlogo.png" height="80"/>  
+      <img src="../quartetlogo.png" height="80" alt="Quartet Logo" />  
       </Grid>
       {currentUser ? (
         <Grid item>
