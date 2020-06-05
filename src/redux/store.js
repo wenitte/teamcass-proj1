ee66e064bdf0bad04ae1594f2db076eae7c5903b
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2020-06-05 16:34:50 +0530

    Add redux-persist to track user session

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
+
 import createSagaMiddleware from "redux-saga";
 import logger from "redux-logger";
 
@@ -17,4 +19,6 @@ const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 sagaMiddleware.run(rootSaga);
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
