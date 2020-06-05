cf8637de27629f4bde54ebb7fcf5f19ba65fdaf8
Pranav Mahesh <pmahesh@gatech.edu>
2020-06-05 11:53:15 -0400

    Add padding and logo for navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,31 +1,28 @@
 import React from "react";
 import { connect } from "react-redux";
-
 import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-
+import './style.css'
 import { signOutStart } from "../../redux/user/user.actions";
 
 const Navbar = ({ signOut, currentUser }) => {
   return (
+    <div class="navbar">
     <Grid justify="space-between" container spacing={3}>
       <Grid item>
-        <Box border={1} borderRadius="50%">
-          <Typography variant="h4">Q</Typography>
-        </Box>
+      <img src="../quartetlogo.png" height="80"/>  
       </Grid>
       {currentUser ? (
         <Grid item>
-          <div>
             <Button variant="contained" color="primary" onClick={() => signOut()}>
               Sign Out
             </Button>
-          </div>
         </Grid>
       ) : null}
     </Grid>
+    </div>
   );
 };
 
